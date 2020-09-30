128dd81cd3f697fe37cdcaf407cadfc0b4f34c6f
aidinazari <70124488+aidinazari@users.noreply.github.com>
2020-09-30 10:08:54 +0330

    Add files via upload

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST = '127.0.0.1'  
+PORT = 65432
+done=False
+
+while not done:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+                print(data)
